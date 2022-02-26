c243a5da8234f8c3fe9bcd1d563602fc62e2c224
ishubham21 <sg2199203@gmail.com>
2022-02-26 18:41:06 +0530

    quiz complete

---
+++ assets/quizzes/questions/crypto.js
@@ -0,0 +1,49 @@
+const crypto = [
+    {
+        question: "Which of these cryptocurrencies is the oldest?",
+        answers: [
+            { text: "Ether", isCorrect: false },
+            { text: "Tether", isCorrect: false },
+            { text: "Dogecoin", isCorrect: true },
+            { text: "Cardano", isCorrect: false }
+        ]
+    },
+    {
+        question: "What is Ethers connection to Ethereum?",
+        answers: [
+            { text: "Same name for same crypto", isCorrect: false },
+            { text: "Ether is currency, Ethereum is blockchain", isCorrect: true },
+            { text: "Ethereum is cryptocurrency, Ether is blockchain", isCorrect: false },
+            { text: "Ether is expensive than Etherium", isCorrect: false }
+        ]
+    },
+    {
+        question: "After Bitcoin, Ether and Tether, which are the biggest crypto coins?",
+        answers: [
+            { text: "Solana", isCorrect: false },
+            { text: "Binance Coin", isCorrect: false },
+            { text: "Cardano", isCorrect: true },
+            { text: "Dogecoin", isCorrect: false }
+        ]
+    },
+    {
+        question: "What is the world’s most actively traded crypto coin?",
+        answers: [
+            { text: "Ether", isCorrect: false },
+            { text: "Tether", isCorrect: true },
+            { text: "Dogecoin", isCorrect: false },
+            { text: "Bitcoin", isCorrect: false }
+        ]
+    },
+    {
+        question: "How much energy does Bitcoin consume annually?",
+        answers: [
+            { text: "More than Greece", isCorrect: false },
+            { text: "More than American Airlines", isCorrect: true },
+            { text: "More than U.S. Federal Government", isCorrect: false },
+            { text: "More than Australia", isCorrect: false }
+        ]
+    },
+]
+
+module.exports = crypto
