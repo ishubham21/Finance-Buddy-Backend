b929c1d49e1ad6c6aab45c0c45075d5234c5ce85
ishubham21 <sg2199203@gmail.com>
2022-02-26 18:05:32 +0530

    adding up lessons

---
+++ assets/quizzes/index.js
@@ -0,0 +1,10 @@
+const sip = require('./questions/sip')
+const mutualFunds = require('./questions/mutualFunds')
+
+const quizzes = {
+    sip,
+    mutualFunds
+}
+
+console.log(quizzes)
+// module.exports = quizzes
---
+++ assets/quizzes/questions/sip.js
@@ -0,0 +1,43 @@
+const sip = [
+    {
+        question: "SIP is a type of mutual fund?",
+        answers: [
+            {text: "True", isCorrect: false},
+            {text: "False", isCorrect: true}
+        ]
+    },
+    {
+        question: "What is the best way offered to set up SIP mode of investing?",
+        answers: [
+            {text: "Make an application each month with a signed cheque", isCorrect: false},
+            {text: "Set up a standing instruction to auto-debit my account each month", isCorrect: true},
+            {text: "Make an application each month and transfer through ECS", isCorrect: false},
+        ]
+    },
+    {
+        question: "Which of these benefits can you get from investing in SIPs?",
+        answers: [
+            {text: "Inculcating financial discipline", isCorrect: false},
+            {text: "Rupee cost averaging", isCorrect: false},
+            {text: "Compounding benefit", isCorrect: false},
+            {text: "All of the above", isCorrect: true}
+        ]
+    },
+    {
+        question: "How is rupee cost averaging beneficial to you as an investor?",
+        answers: [
+            {text: "Gives you assured returns", isCorrect: false},
+            {text: "Eliminates the need to time the market", isCorrect: true},
+            {text: "Increases the cost of investment", isCorrect: true}
+        ]
+    },
+    {
+        question: "Which of the following is true about quantum of SIP investing?",
+        answers: [
+            {text: "SIP amount each month remains fixed and cannot be changed", isCorrect: false},
+            {text: "SIP amounts can be altered as well as stopped during its tenure", isCorrect: true},
+        ]
+    }
+]
+
+module.exports = sip
